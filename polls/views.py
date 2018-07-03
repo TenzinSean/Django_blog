65cb4d3f3b26ca82154d6b66c99a5723d07d0acf
Tenzin <Tenzin.jeunesse@gmail.com>
2018-07-03 11:16:18 +0200

    added

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Job
+
+
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
